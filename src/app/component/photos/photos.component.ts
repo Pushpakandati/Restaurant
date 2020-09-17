52212237a47cb2a8acc36360c6271d01a4e154f7
Pushpa kandati <sai.anju1994@gmail.com>
2020-09-17 23:27:21 +0530

    updated

--- src/app/component/photos/photos.component.ts
+++ src/app/component/photos/photos.component.ts
@@ -7,11 +7,20 @@ import { RestaurantService } from './../../restaurant.service';
 })
 export class PhotosComponent implements OnInit {
  imgUrl:any;
+ restaurantInfo:any;
+ restaurantCloneData:any;
   constructor(private resService:RestaurantService) { }
 
   ngOnInit(): void {
+  	this.restaurantData();
   	this.imgUrl=this.resService.getImagePath();
   	 console.log(this.resService.getImagePath(),'image');
   }
-
+  restaurantData(){
+    this.resService.getRestaurantData().subscribe(data => {
+      this.restaurantInfo=data;
+      this.restaurantCloneData=Object.values(this.restaurantInfo)[0];
+      console.log(this.restaurantCloneData,'restaurantCloneData')
+      })
+  }
 }
